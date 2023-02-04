f02e040de478e918b1630ec4d8c326dc0615911a
Revi1337 <david122123@gmail.com>
2023-02-04 19:46:55 +0900

    Paging
    페이징 처리를 위해 테스트코드에서 300개의 row 를 생성

--- src/test/java/com/example/jumptospringboot/JumpToSpringBootApplicationTests.java
+++ src/test/java/com/example/jumptospringboot/JumpToSpringBootApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.jumptospringboot;
 
+import com.example.jumptospringboot.service.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JumpToSpringBootApplicationTests {
 
+    @Autowired
+    private QuestionService questionService;
+
     @Test
-    void contextLoads() {
+    void testJpa() {
+        for (int i = 1; i < 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            questionService.create(subject, content);
+        }
     }
 
 }
