d2332c2c94be346cf6c93b5f8594ec476b826a27
Revi1337 <david122123@gmail.com>
2023-02-05 04:16:00 +0900

    writter
    글쓴이 표시

--- src/main/java/com/example/jumptospringboot/controller/AnswerController.java
+++ src/main/java/com/example/jumptospringboot/controller/AnswerController.java
@@ -9,7 +9,6 @@ import com.example.jumptospringboot.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
